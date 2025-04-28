576ea3f6664b081e9a013e01800a6244415c6cd8
leo <caoliu1994@gmail.com>
2025-04-28 14:59:21 +0800

    date

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from WeChatSpider import daily_scrape
 import datetime
 import time
 
+print("Starting the scraper...")
 while True:
     if datetime.datetime.now().hour in [10,15,22] and datetime.datetime.now().minute < 10:
         try:
@@ -9,4 +10,5 @@ while True:
         except Exception as e:
             print(e)
     time.sleep(60)
+    print("Waiting for the next run...")
     
